f74396584ada213a39d63f5b01b9fe8885528774
mwong01 <mattw90@gmail.com>
2019-11-26 09:53:11 -0500

    create confirm, error, show and status appointment views

--- stories/index.js
+++ stories/index.js
@@ -1,25 +1,19 @@
 import React from "react";
-
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-
 import "index.scss";
-
 import Button from "components/Button";
-
 import DayListItem from "components/DayListItem";
-
 import DayList from "components/DayList";
-
 import InterviewerListItem from "components/InterviewerListItem";
-
 import InterviewerList from "components/InterviewerList";
-
 import Appointment from "components/Appointment/index";
-
 import Header from "components/Appointment/Header";
-
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 import "components/Appointment/styles.scss";
 
@@ -162,4 +156,8 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   // header story
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+  .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
+  .add("Status", () => <Status message="Deleting"/>)
+  .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")}/>)
