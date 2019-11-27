a40fa775193ef4be5c52e3b8c651ed486a5080be
mwong01 <mattw90@gmail.com>
2019-11-26 20:10:06 -0500

    add proxy to json and request days script

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,23 +4,23 @@ import "components/Application.scss";
 import Appointment from "components/Appointment";
 const axios = require('axios');
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = [
   {
@@ -79,6 +79,15 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days")
+    .then((response) => {
+      setDays(response.data);
+    })
+  }, []);
+
   const appointmentComponents = appointments.map(appointment =>{
     return (
       <Appointment key={appointment.id} {...appointment} />
